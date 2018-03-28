0229cf3a84321c452ce5f132541f65c439a7f96b
Albert Visser <albert@lemoncurry>
2018-03-28 20:17:18 +0200

    changed managing gui toolkit used

--- apropos/a_propos.py
+++ apropos/a_propos.py
@@ -1,24 +1,25 @@
 """start a-propos app
 
-argumenten: filenaam, toolkit, venstertitel
+argumenten: filenaam, venstertitel indien gewenst
 """
+from .toolkit import toolkit
 
-
-def apropos(file='', toolkit='qt', title=''):
+def apropos(file='', title=''):
     """start de GUI op.
 
     Importeert hiervoor de toolkit-specifieke code
     """
+    print(toolkit)
     if toolkit == 'qt4':
         from .apropos_qt4 import main
     elif toolkit == 'qt':
         from .apropos_qt5 import main
+    elif toolkit == 'gtk':
+        from .apropos_gtk3 import main
     elif toolkit == 'wx':
         from .apropos_wx import main
     else:
         raise ValueError('Unknown GUI-toolkit specification: '
-                         'currently only `qt(5)` and `wx` are supported')
+                         'currently only `qt(5)`, `gtk` and `wx` are supported')
     main(file=file, title=title)
 
-if __name__ == "__main__":
-    apropos()
