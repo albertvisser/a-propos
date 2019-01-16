fe5bf1b1212d1ad5672161c95c3e69bc657f6de0
Albert Visser <albert@lemoncurry>
2019-01-16 19:07:53 +0100

    #770: sneltoetsen via accelerator table

--- apropos/apropos_wx.py
+++ apropos/apropos_wx.py
@@ -18,12 +18,12 @@ class Page(wx.Panel):
     "Panel subclass voor de notebook pagina's"
     def __init__(self, parent, id, mf):
         wx.Panel.__init__(self, parent, id)
-        self.txt = wx.TextCtrl(self, -1, style=wx.TE_MULTILINE | wx.BORDER_NONE, size=DFLT_SIZE)
-        self.txt.Bind(wx.EVT_KEY_DOWN, mf.on_key)
+        self.txt = wx.TextCtrl(self, -1, style=wx.TE_MULTILINE, size=DFLT_SIZE)
+        # self.txt.Bind(wx.EVT_KEY_DOWN, mf.on_key)
         ## self.Bind(wx.EVT_TEXT, self.OnEvtText, self.txt)
         sizer0 = wx.BoxSizer(wx.VERTICAL)
         # sizer1 = wx.BoxSizer(wx.HORIZONTAL)
-        sizer0.Add(self.txt, 1, wx.EXPAND | wx.ALL, 5)
+        sizer0.Add(self.txt, 1, wx.EXPAND | wx.ALL, 10)
         # sizer0.Add(sizer1, 1, wx.EXPAND)
         self.SetSizer(sizer0)
         # self.SetAutoLayout(True)
@@ -75,22 +75,43 @@ class MainFrame(wx.Frame, ApoMixin):
         pnl = self  # wx.Panel(self, -1)
         self.nb = wx.Notebook(pnl, -1)
         self.nb.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.page_changed)
-        self.nb.Bind(wx.EVT_LEFT_DCLICK, self.on_left_doubleclick)
-        self.nb.Bind(wx.EVT_MIDDLE_DOWN, self.on_left_doubleclick)
-        self.nb.Bind(wx.EVT_LEFT_UP, self.on_left_release)
-        self.nb.Bind(wx.EVT_KEY_DOWN, self.on_key)
+        ## self.nb.Bind(wx.EVT_LEFT_DCLICK, self.on_left_doubleclick)
+        ## self.nb.Bind(wx.EVT_MIDDLE_DOWN, self.on_left_doubleclick)
+        ## self.nb.Bind(wx.EVT_LEFT_UP, self.on_left_release)
+        # self.nb.Bind(wx.EVT_KEY_DOWN, self.on_key)
+        accel_list = []
+        for label, shortcuts, handler in (
+                ('reload', ('Ctrl+R',), self.load_data),
+                ('newtab', ('Ctrl+N',), self.newtab),
+                ('close', ('Ctrl+W',), self.closetab),
+                ('hide', ('Ctrl+H',), self.hide_app),
+                ('save', ('Ctrl+S',), self.save_data),
+                ('quit', ('Ctrl+Q', 'Escape'), self.close),
+                ('language', ('Ctrl+L',), self.choose_language),
+                ('next', ('Alt+Right',), self.goto_next),
+                ('prev', ('Alt+Left',), self.goto_previous),
+                ('help', ('F1',), self.helppage),
+                ('title', ('F2',), self.asktitle), ):
+            menuitem = wx.MenuItem(None, -1, label)
+            self.Bind(wx.EVT_MENU, handler, menuitem)
+            for key in shortcuts:
+                accel = wx.AcceleratorEntry(cmd=menuitem.GetId())
+                ok = accel.FromString(key)
+                accel_list.append(accel)
+        accel_table = wx.AcceleratorTable(accel_list)
+        self.SetAcceleratorTable(accel_table)
         self.set_apofile(fname)
         self.initapp()
         sizer0 = wx.BoxSizer(wx.VERTICAL)
         # sizer1 = wx.BoxSizer(wx.HORIZONTAL)
-        sizer0.Add(self.nb, 1, wx.EXPAND | wx.ALL, 10)
+        sizer0.Add(self.nb, 1, wx.EXPAND)  # | wx.ALL, 10)
         # sizer0.Add(sizer1, 1, wx.EXPAND)
         pnl.SetSizer(sizer0)
         # pnl.SetAutoLayout(True)
         # sizer0.Fit(pnl)
         sizer0.SetSizeHints(pnl)
         # pnl.Layout()
-        self.Bind(wx.EVT_CLOSE, self.afsl)
+        self.Bind(wx.EVT_CLOSE, self.close)
         self.Show()
 
     def page_changed(self, event=None):
@@ -158,14 +179,14 @@ class MainFrame(wx.Frame, ApoMixin):
         self.closetab(item)
         event.Skip()
 
-    def load_data(self):
+    def load_data(self, event=None):
         """get data and setup notebook
         """
         self.nb.DeleteAllPages()
         self.initapp()
         self.confirm(setting="NotifyOnLoad", textitem="load_text")
 
-    def hide_app(self):
+    def hide_app(self, event=None):
         """minimize to tray
         """
         self.confirm(setting="AskBeforeHide", textitem="hide_text")
@@ -175,7 +196,7 @@ class MainFrame(wx.Frame, ApoMixin):
         self.tbi.Bind(wx.EVT_TASKBAR_RIGHT_UP, self.revive)
         self.Hide()
 
-    def save_data(self):
+    def save_data(self, event=None):
         """update persistent storage
         """
         self.afsl()
@@ -200,7 +221,7 @@ class MainFrame(wx.Frame, ApoMixin):
             self.newtab()
             self.current = 0
 
-    def newtab(self, titel=None, note=None):
+    def newtab(self, event=None, titel=None, note=None):
         """initialiseer een nieuwe tab
         """
         nieuw = self.nb.GetPageCount()
@@ -212,7 +233,13 @@ class MainFrame(wx.Frame, ApoMixin):
         self.nb.AddPage(newpage, titel)
         self.nb.SetSelection(nieuw)
 
-    def closetab(self, pagetodelete=None):
+    def goto_previous(self, event=None):
+        self.nb.AdvanceSelection(False)
+
+    def goto_next(self, event=None):
+        self.nb.AdvanceSelection()
+
+    def closetab(self, event=None, pagetodelete=None):
         """sluit de huidige of aangegeven tab
         """
         if pagetodelete is None:
@@ -230,27 +257,25 @@ class MainFrame(wx.Frame, ApoMixin):
         self.Show()
         self.tbi.Destroy()
 
-    def afsl(self, event=None):
+    def close(self, event=None):
+        self.afsl()
+        self.quitting = True
+        self.nb.DeleteAllPages()
+        self.Destroy()
+
+    def afsl(self):
         """applicatiedata opslaan voorafgaand aan afsluiten
         """
-        print("in MainFrame.afsl")
         self.opts["ActiveTab"] = self.nb.GetSelection()
-        print("   got selection")
         self.apodata = {0: self.opts}
         for i in range(self.nb.GetPageCount()):
-            print('    getting page', i)
             page = self.nb.GetPage(i)
             title = self.nb.GetPageText(i)
             text = page.txt.GetValue()
             self.apodata[i + 1] = (title, text)
-        print('saving notes')
         self.save_notes(self.apodata)
-        if event:
-            print('    skip event handling')
-            event.Skip()
-        print('end of MainFrame.afsl')
 
-    def helppage(self):
+    def helppage(self, event=None):
         """vertoon de hulp pagina met keyboard shortcuts
         """
         dlg = wx.MessageDialog(self, languages[self.opts["language"]]["info"],
@@ -269,7 +294,7 @@ class MainFrame(wx.Frame, ApoMixin):
                 self.opts[setting] = False
             dlg.Destroy()
 
-    def asktitle(self):
+    def asktitle(self, event=None):
         """toon dialoog om tab titel in te vullen/aan te passen en verwerk antwoord
         """
         dlg = wx.TextEntryDialog(self, languages[self.opts["language"]]["ask_title"],
@@ -278,7 +303,7 @@ class MainFrame(wx.Frame, ApoMixin):
             self.nb.SetPageText(self.current, dlg.GetValue())
         dlg.Destroy()
 
-    def choose_language(self):
+    def choose_language(self, event=None):
         """toon dialoog om taal te kiezen en verwerk antwoord
         """
         data = [(x, y["language"]) for x, y in languages.items()]
