c57af04ccb534083233d81f532f391904e9f5d8e
Albert Visser <albert.visser@gmail.com>
2023-12-23 21:16:02 +0100

    argumenten iets gewijzigd

--- start.py
+++ start.py
@@ -7,6 +7,6 @@ from apropos.main import apropos
 parser = argparse.ArgumentParser(description='Simple notes application')
 parser.add_argument('-f', '--file', help='path to the data file (defaults to '
                                          '`apropos.apo` in the startup directory)')
-parser.add_argument('-n', '--title', help="provide a title for the window")
+parser.add_argument('-t', '--title', help="provide a title for the window")
 args = parser.parse_args()
 apropos(fname=args.file, title=args.title)
